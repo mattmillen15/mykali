d047c9c3c536451352743bbbfedc77cd849b59e9
mattmillen15 <68832392+mattmillen15@users.noreply.github.com>
2024-12-26 13:21:15 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -29,6 +29,9 @@ sudo apt install -y \
     bat \
     fzf
 
+git clone https://github.com/tmux-plugins/tpm.git ~/mykali/tmux/plugins/.
+git clone https://github.com/catppuccin/tmux.git ~/mykali/tmux/plugins/catppuccin/tmux
+
 #######################################################
 # TOOLS INSTALLATION (via tools.sh)
 #######################################################
